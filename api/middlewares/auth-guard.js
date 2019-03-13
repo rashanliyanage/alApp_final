d0420acac68edcb87e889a940bc77d6d2092d9a7
Dinusha Naveen <dldndasanayaka@gmail.com>
2019-03-13 15:26:58 +0530

    edit auth guard

--- api/middlewares/auth-guard.js
+++ api/middlewares/auth-guard.js
@@ -4,10 +4,29 @@ function decode(req, res, next){
     const token = req.headers.authorization.split(" ")[1];
     const decodeJWT = jwt.verify(token, process.env.JWT_KEY);
     console.log(decodeJWT);
-    next()
     return decodeJWT;
 }
 
+function checkIfAdmin(req, res, next) {
+    try {
+        const decodeJWT = decode(req, res, next);
+        if (decodeJWT.user.role === 'admin'){
+            console.log("admin")
+            next()
+        } else{
+            return res.status(200).json({
+                state: 10,
+                Message: 'Oops!.... Not Enough Permissions'
+            });
+        }
+    } catch (error) {
+        res.status(401).json({
+            state: false
+        })
+    }
+}
+
 module.exports = {
-    decode: decode
+    decode: decode,
+    checkIfAdmin: checkIfAdmin
 }
